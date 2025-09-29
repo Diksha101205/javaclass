c3b44a1f3a81938505585fecf3240c6dd99ee054
Diksha <Dikshatanwar56@gmail.com>
2025-09-29 11:52:15 +0530

    Update excptn.java

--- excptn.java
+++ excptn.java
@@ -1,9 +1,12 @@
 package class8;
-
+import java.util.Scanner;
 public class excptn {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         try{
-            int result = 10/0;
+            int result = a/b;
             System.out.println(result);
         }
         catch(ArithmeticException e){
@@ -11,3 +14,4 @@ public class excptn {
         }
     }
 }
+
